53d57c36bb0519c772dc9c47ca0f05fe54a20461
Arif Khan <arifkhan.incaendo@gmail.com>
2019-09-13 11:22:28 +0530

    Consume store

--- src/components/new-todo-form/index.js
+++ src/components/new-todo-form/index.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class NewTodoForm extends Component {
 	state = {
 		todo: '',
+		error: '',
 	}
 
 	onSubmit = (e) => {
 		e.preventDefault();
-		console.log(this.state);
+
 	}
 
 	handleOnChange = (e) => {
+		if (!e.target.value) {
+			this.setState({
+				error: "Todo can not be blank!"
+			});
+			return;
+		}
+
 		this.setState({
 			todo: e.target.value,
+			error: ''
 		})
 	}
 
@@ -22,14 +32,28 @@ class NewTodoForm extends Component {
 				<form id="new-todo-form" className="w-full max-w-sm p-2" onSubmit={this.props.onSubmit || this.onSubmit} data-testid="new-todo-form">
 					<div className="flex item-center border-b border-b-2 border-blue-500 py-2">
 						<input type="text" onChange={this.handleOnChange} name="new-todo" className="appearence-none bg-transparent border-none w-full text-grey-700 mr-3 py-1 px-2 leading-tight focus:outline-none"/>
-						<button type="submit" className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+						<button type="submit" className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded">
 							ADD
 						</button>
 					</div>
+					{
+						this.state.error.length !== 0 &&
+						(
+							<p className="text-red-500 text-xs italic mt-3">{ this.state.error }</p>
+						)
+					}
 				</form>
       </div>
     )
   }
 }
 
-export default NewTodoForm;
+const mapDispatchToProps = dispatch => {
+  return {}
+}
+
+const mapStateToProps = state => {
+	console.log(state);
+	return {};
+}
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
