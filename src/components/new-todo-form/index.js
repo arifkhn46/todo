8388489ce4881d4d484b3ffbcc4606354d05b868
Arif Khan <arifkhan.incaendo@gmail.com>
2019-09-13 11:40:32 +0530

    Consume store in the component

--- src/components/new-todo-form/index.js
+++ src/components/new-todo-form/index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { addNewTodo } from 'store/actions/newTodo';
+
 class NewTodoForm extends Component {
-	state = {
-		todo: '',
-		error: '',
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			newTodo: this.props.newTodo,
+			error: '',
+		}
 	}
 
 	onSubmit = (e) => {
 		e.preventDefault();
-
+		this.props.addNewTodo(this.state.todo);
 	}
 
 	handleOnChange = (e) => {
@@ -21,7 +27,9 @@ class NewTodoForm extends Component {
 		}
 
 		this.setState({
-			todo: e.target.value,
+			newTodo: {
+				title: e.target.value
+			},
 			error: ''
 		})
 	}
@@ -31,7 +39,7 @@ class NewTodoForm extends Component {
       <div className='new-todo-form'>
 				<form id="new-todo-form" className="w-full max-w-sm p-2" onSubmit={this.props.onSubmit || this.onSubmit} data-testid="new-todo-form">
 					<div className="flex item-center border-b border-b-2 border-blue-500 py-2">
-						<input type="text" onChange={this.handleOnChange} name="new-todo" className="appearence-none bg-transparent border-none w-full text-grey-700 mr-3 py-1 px-2 leading-tight focus:outline-none"/>
+						<input type="text" onChange={this.handleOnChange} name="new-todo" className="appearence-none bg-transparent border-none w-full text-grey-700 mr-3 py-1 px-2 leading-tight focus:outline-none" value={this.state.newTodo.title}/>
 						<button type="submit" className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded">
 							ADD
 						</button>
@@ -49,11 +57,15 @@ class NewTodoForm extends Component {
 }
 
 const mapDispatchToProps = dispatch => {
-  return {}
+  return {
+		addNewTodo: (newTodo) => dispatch(addNewTodo(newTodo))
+	}
 }
 
 const mapStateToProps = state => {
 	console.log(state);
-	return {};
+	return {
+		newTodo: state.newTodo
+	};
 }
 export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
